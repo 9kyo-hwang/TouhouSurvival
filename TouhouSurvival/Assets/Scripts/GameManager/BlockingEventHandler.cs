1b3879694452429637a40a0179ae60aa450761f8
limeLimelimeLime <nlime3141592@naver.com>
2025-01-18 00:40:36 +0900

    [Feature] Unity Coroutine 기반 Blocking Event 구현 및 테스트 완료

---
+++ TouhouSurvival/Assets/Scripts/GameManager/BlockingEventHandler.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Unchord
+{
+    public class BlockingEventHandler : MonoBehaviour
+    {
+        public event Action onBlockingEventOccurred;
+        public event Action onBlockingEventHandled;
+
+        private Queue<IEnumerator> _eventHandlers;
+        public float _handlingCooldown;
+        public bool _eventEnabled;
+
+        public void Publish(IEnumerator eventHandler)
+        {
+            _eventHandlers.Enqueue(eventHandler);
+        }
+
+        private void Awake()
+        {
+            _eventHandlers = new Queue<IEnumerator>(16);
+        }
+
+        private void Update()
+        {
+            if (_eventEnabled)
+                return;
+            else if (_handlingCooldown > 0.0f)
+                _handlingCooldown -= Time.deltaTime;
+            else if (_eventHandlers.Count > 0)
+                StartCoroutine(HandleEventCoroutine(_eventHandlers.Dequeue()));
+        }
+
+        private IEnumerator HandleEventCoroutine(IEnumerator eventHandler)
+        {
+            _eventEnabled = true;
+            onBlockingEventOccurred?.Invoke();
+            yield return StartCoroutine(eventHandler);
+            _handlingCooldown = 1.0f;
+            _eventEnabled = false;
+            onBlockingEventHandled?.Invoke();
+        }
+    }
+}
