2773508951d1c10fb9fb5dca8c6bc992775cdd6f
limeLimelimeLime <nlime3141592@naver.com>
2025-01-22 02:48:54 +0900

    [Refactor] Unchord Utility 클래스 생성

--- TouhouSurvival/Assets/Scripts/GameManager/ObjectDictionary.cs
+++ TouhouSurvival/Assets/Scripts/GameManager/ObjectDictionary.cs
@@ -19,7 +19,7 @@ namespace Unchord
 
         public void Add(GameObject gameObject, int chunkPositionX, int chunkPositionY)
         {
-            int key = PointToIndex(chunkPositionX, chunkPositionY);
+            int key = UnchordUtility.PointToIndex(chunkPositionX, chunkPositionY);
 
             if (!_dictionary.ContainsKey(key))
                 _dictionary.Add(key, new List<GameObject>(INITIAL_CHUNK_CAPACITY));
@@ -33,7 +33,7 @@ namespace Unchord
 
         public void Remove(GameObject gameObject, int chunkPositionX, int chunkPositionY)
         {
-            int key = PointToIndex(chunkPositionX, chunkPositionY);
+            int key = UnchordUtility.PointToIndex(chunkPositionX, chunkPositionY);
 
             if (_dictionary.ContainsKey(key) && _dictionary[key].Contains(gameObject))
             {
@@ -43,7 +43,7 @@ namespace Unchord
 
         public void Enable(int chunkPositionX, int chunkPositionY)
         {
-            int key = PointToIndex(chunkPositionX, chunkPositionY);
+            int key = UnchordUtility.PointToIndex(chunkPositionX, chunkPositionY);
 
             if (_enableChunkList.ContainsKey(key) || !_dictionary.ContainsKey(key))
                 return;
@@ -60,7 +60,7 @@ namespace Unchord
 
         public void Disable(int chunkPositionX, int chunkPositionY)
         {
-            int index = PointToIndex(chunkPositionX, chunkPositionY);
+            int index = UnchordUtility.PointToIndex(chunkPositionX, chunkPositionY);
 
             if (!_enableChunkList.ContainsKey(index) || !_dictionary.ContainsKey(index))
                 return;
@@ -72,82 +72,6 @@ namespace Unchord
             for (int i = list.Count - 1; i >= 0; --i)
             {
                 list[i].SetActive(false);
-            }
-        }
-
-        public static int PointToIndex(int x, int y)
-        {
-            Debug.Assert(x >= -11584 && x <= 11585);
-            Debug.Assert(y >= -11584 && y <= 11585);
-
-            int transposedFlag = 1;
-
-            if (x > y)
-            {
-                int temp = x;
-                x = y;
-                y = temp;
-                transposedFlag = -1;
-            }
-
-            if (x + y > 0)
-            {
-                int pivot = y + y;
-                pivot = pivot * (pivot - 1);
-                return pivot + transposedFlag * (y - x);
-            }
-            else
-            {
-                int pivot = x + x;
-                pivot = pivot * (pivot - 1);
-                return pivot + transposedFlag * (x - y);
-            }
-        }
-
-        public static void IndexToPoint(int index, out int x, out int y)
-        {
-            // NOTE:
-            // 23170 == 11585 - (-11584) + 1
-            // 536848899 == (23170)^2 - 1
-            Debug.Assert(index >= 0 && index <= 536848899);
-
-            // NOTE: The variable 'v' is derived value from 'index'.
-            int v = (int)Math.Floor(Math.Sqrt(4 * index + 1));
-            int r = v % 4;
-            int n = default;
-            int pivot = default;
-
-            switch(r)
-            {
-                case 0:
-                    n = v / 4;
-                    pivot = PointToIndex(-n, -n);
-                    x = -n;
-                    y = -n + pivot - index;
-                    break;
-                case 1:
-                    n = (v - 1) / 4;
-                    pivot = PointToIndex(-n, -n);
-                    x = -n - pivot + index;
-                    y = -n;
-                    break;
-                case 2:
-                    n = (v - 2) / 4;
-                    pivot = PointToIndex(n + 1, n + 1);
-                    x = n + 1;
-                    y = n + 1 - pivot + index;
-                    break;
-                case 3:
-                    n = (v + 1) / 4;
-                    pivot = PointToIndex(n, n);
-                    x = n + pivot - index;
-                    y = n;
-                    break;
-                default:
-                    x = default;
-                    y = default;
-                    Debug.Assert(false, $"Unknown case found. Please debug. (case {r})");
-                    break;
             }
         }
     }
