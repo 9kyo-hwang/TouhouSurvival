e7f1e0b66cc74327e3afbcb060b97846343551ee
limeLimelimeLime <nlime3141592@naver.com>
2025-01-23 02:33:48 +0900

    [Fix] 환경설정 화면의 Mute 버튼이 오동작하는 현상 수정

--- TouhouSurvival/Assets/Scripts/GUI/SettingsCanvas.cs
+++ TouhouSurvival/Assets/Scripts/GUI/SettingsCanvas.cs
@@ -11,22 +11,20 @@ namespace Unchord
         private Slider _sliderSfx;
         private Button _btnBgmMute;
         private Button _btnSfxMute;
-        private TextMeshProUGUI _txtBgmAmp;
-        private TextMeshProUGUI _txtSfxAmp;
+        private TextMeshProUGUI _txtBgm;
+        private TextMeshProUGUI _txtSfx;
 
         protected override void Awake()
         {
             _btnBack = transform.Find("SettingsPanel/BackButton").GetComponent<Button>();
             _sliderBgm = transform.Find("SettingsPanel/BgmSlider").GetComponent<Slider>();
             _sliderSfx = transform.Find("SettingsPanel/SfxSlider").GetComponent<Slider>();
-            _btnBgmMute = _sliderBgm.transform.Find("MuteButton").GetComponent<Button>();
-            _btnSfxMute = _sliderSfx.transform.Find("MuteButton").GetComponent<Button>();
-            _txtBgmAmp = _sliderBgm.transform.Find("VolumeLabel/ContentText").GetComponent<TextMeshProUGUI>();
-            _txtSfxAmp = _sliderSfx.transform.Find("VolumeLabel/ContentText").GetComponent<TextMeshProUGUI>();
+            _btnBgmMute = transform.Find("SettingsPanel/BgmMuteButton").GetComponent<Button>();
+            _btnSfxMute = transform.Find("SettingsPanel/SfxMuteButton").GetComponent<Button>();
+            _txtBgm = _sliderBgm.transform.Find("VolumeLabel/ContentText").GetComponent<TextMeshProUGUI>();
+            _txtSfx = _sliderSfx.transform.Find("VolumeLabel/ContentText").GetComponent<TextMeshProUGUI>();
 
             _btnBack.onClick.AddListener(OnBackButtonClick);
-            _sliderBgm.onValueChanged.AddListener(OnBgmSliderValueChanged);
-            _sliderSfx.onValueChanged.AddListener(OnSfxSliderValueChanged);
             _btnBgmMute.onClick.AddListener(OnBgmMuteButtonClick);
             _btnSfxMute.onClick.AddListener(OnSfxMuteButtonClick);
         }
@@ -38,10 +36,22 @@ namespace Unchord
             SoundChannel chanBgm = SoundManager.Instance.BGM;
             SoundChannel chanSfx = SoundManager.Instance.SFX;
 
-            MoveSliderHandle(chanBgm, _sliderBgm);
-            MoveSliderHandle(chanSfx, _sliderSfx);
-            RenderVolumeText(chanBgm, _sliderBgm, _txtBgmAmp);
-            RenderVolumeText(chanSfx, _sliderSfx, _txtSfxAmp);
+            _sliderBgm.value = GetSliderVolume(chanBgm, _sliderBgm);
+            _sliderSfx.value = GetSliderVolume(chanSfx, _sliderSfx);
+
+            _sliderBgm.onValueChanged.AddListener(OnBgmSliderValueChanged);
+            _sliderSfx.onValueChanged.AddListener(OnSfxSliderValueChanged);
+
+            _txtBgm.text = GetVolumeString(_sliderBgm.value);
+            _txtSfx.text = GetVolumeString(_sliderSfx.value);
+        }
+
+        public override void Hide()
+        {
+            base.Hide();
+
+            _sliderBgm.onValueChanged.RemoveListener(OnBgmSliderValueChanged);
+            _sliderSfx.onValueChanged.RemoveListener(OnSfxSliderValueChanged);
         }
 
         private void OnBackButtonClick()
@@ -54,13 +64,13 @@ namespace Unchord
         {
             SoundChannel channel = SoundManager.Instance.BGM;
 
-            float volume = value / _sliderBgm.maxValue;
-            Debug.Assert(volume >= 0.0f && volume <= 1.0f, "Volume should be in range between 0 and 1");
+            float volume = GetChannelVolume(_sliderBgm);
 
             // NOTE: 코드 순서 바뀌지 않도록 주의.
-            channel.ChangeVolume(volume);
+            channel.Volume = volume;
             channel.IsMuted = false;
-            RenderVolumeText(channel, _sliderBgm, _txtBgmAmp);
+
+            _txtBgm.text = GetVolumeString(_sliderBgm.value);
         }
 
         private void OnSfxSliderValueChanged(float value)
@@ -68,45 +78,64 @@ namespace Unchord
             SoundChannel channel = SoundManager.Instance.SFX;
 
             float volume = value / _sliderSfx.maxValue;
-            Debug.Assert(volume >= 0.0f && volume <= 1.0f, "Volume should be in range between 0 and 1");
 
             // NOTE: 코드 순서 바뀌지 않도록 주의.
-            channel.ChangeVolume(volume);
+            channel.Volume = volume;
             channel.IsMuted = false;
-            RenderVolumeText(channel, _sliderSfx, _txtSfxAmp);
+
+            _txtSfx.text = GetVolumeString(_sliderSfx.value);
         }
 
         private void OnBgmMuteButtonClick()
         {
             SoundChannel channel = SoundManager.Instance.BGM;
 
-            ToggleMute(channel, _sliderBgm);
-            RenderVolumeText(channel, _sliderBgm, _txtBgmAmp);
+            _sliderBgm.onValueChanged.RemoveListener(OnBgmSliderValueChanged);
+            ToggleMute(channel, _sliderBgm, _txtBgm);
+            _sliderBgm.onValueChanged.AddListener(OnBgmSliderValueChanged);
         }
 
         private void OnSfxMuteButtonClick()
         {
             SoundChannel channel = SoundManager.Instance.SFX;
 
-            ToggleMute(channel, _sliderSfx);
-            RenderVolumeText(channel, _sliderSfx, _txtSfxAmp);
+            _sliderSfx.onValueChanged.RemoveListener(OnSfxSliderValueChanged);
+            ToggleMute(channel, _sliderSfx, _txtSfx);
+            _sliderSfx.onValueChanged.AddListener(OnSfxSliderValueChanged);
         }
 
-        private void ToggleMute(SoundChannel channel, Slider slider)
+        private void ToggleMute(SoundChannel channel, Slider slider, TextMeshProUGUI textComponent)
         {
             channel.IsMuted ^= true;
-            MoveSliderHandle(channel, slider);
+
+            float sliderVolume = GetSliderVolume(channel, slider);
+            slider.value = sliderVolume;
+            textComponent.text = GetVolumeString(sliderVolume);
         }
 
-        private void RenderVolumeText(SoundChannel channel, Slider slider, TextMeshProUGUI textComponent)
+        private string GetVolumeString(float sliderVolume)
         {
-            int discreteVolume = Mathf.RoundToInt(channel.Volume * slider.maxValue);
-            textComponent.text = string.Format("{0}", discreteVolume);
+            return $"{sliderVolume}";
         }
 
-        private void MoveSliderHandle(SoundChannel channel, Slider slider)
+        private float GetSliderVolume(SoundChannel channel, Slider slider)
         {
-            slider.value = channel.Volume * slider.maxValue;
+            float channelVolume = channel.Volume;
+
+            if (!channel.IsMuted && channel.IsPaused)
+                channelVolume = channel.BufferedVolume;
+
+            float range = slider.maxValue - slider.minValue;
+
+            return Mathf.RoundToInt(slider.minValue + channelVolume * range);
+        }
+
+        private float GetChannelVolume(Slider slider)
+        {
+            float range = slider.maxValue - slider.minValue;
+            float value = slider.value - slider.minValue;
+
+            return value / range;
         }
     }
 }
