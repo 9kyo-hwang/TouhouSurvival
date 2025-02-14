dc21a25520fc6b7b5abf5baada1f596ebe0ab41f
YellowGreenLime <nlime3141592@naver.com>
2025-02-15 06:05:14 +0900

    [Feature] 어빌리티 레벨 속성 추가

--- TouhouSurvival/Assets/Scripts/Ability/AbilityComponent.cs
+++ TouhouSurvival/Assets/Scripts/Ability/AbilityComponent.cs
@@ -26,12 +26,22 @@ namespace Unchord
 
         }
 
+        public int Level
         {
+            get => _level;
+            set => SetLevel(value);
         }
 
+        public float NormalizedLevel => (float)_level / maxLevel;
+
+        public int maxLevel = 1;
+
         protected Player _player { get; private set; }
+        private int _level;
+        
         protected virtual void Awake()
         {
+
         }
 
         protected virtual void Update()
@@ -42,6 +52,32 @@ namespace Unchord
         public void Subscribe(Player player)
         {
             _player = player;
+        }
+
+        private void SetLevel(int level)
+        {
+            Debug.Assert(level >= 0);
+
+            int prevLevel = _level;
+            int nextLevel = level;
+
+            for (int i = _level + 1; i <= level; ++i)
+            {
+                // TODO: Write code for updating player's stat. (level increasing)
+            }
+
+            for (int i = _level; i > level; --i)
+            {
+                // TODO: Write code for updating player's stat. (level decreasing)
+            }
+
+            _level = level;
+
+            if (prevLevel != nextLevel)
+            {
+                // TODO: Enable this comment after integrating region Ability Pool Management from this class to Player class.
+                // _player.OnChangeAbilityLevel(this, prevLevel, nextLevel);
+            }
         }
     }
 }
