fe9fb67d4989eb83ec5662d829235baa38c2443f
9kyo-hwang <ryghkd507@naver.com>
2025-03-27 14:54:01 +0900

    [Feature] Controller-Weapon-Effect 구조 설계 중

--- TouhouSurvival/Assets/Scripts/Ability/Weapons/Harudesuyo/Harudesuyo.cs
+++ TouhouSurvival/Assets/Scripts/Ability/Weapons/Harudesuyo/Harudesuyo.cs
@@ -1,52 +1,83 @@
+using System;
 using System.Collections;
-using System.Linq;
-using Unchord;
 using UnityEngine;
+using UnityEngine.Pool;
+using UnityEngine.Serialization;
 
 namespace Unchord
 {
-    public class Harudesuyo : WeaponComponent
+    public class Harudesuyo : MonoBehaviour
     {
-        [Header("Prefab Settings")]
-        public GameObject harudesuyoPrefab;
-
-        private float _cooldown;
-
-        protected override void Awake()
-        {
-            base.Awake();
-            
-            _cooldown = Attributes[HarudesuyoAttributeType.Cooldown].CurrentValue;
-        }
-
-        protected override void Update()
-        {
-            base.Update();
-        }
-
-        protected override void UseWeapon()
-        {
-            base.UseWeapon();
-            
-        }
+        private float _fallTime;
+        private float _radius;
+        private float _damage;
+        private Transform _target;
+        private ObjectPool<Harudesuyo> _pool;           // 생성 후 Pool에 반환시키기 위해 참조만 들고 있음
+        private ObjectPool<GameObject> _explosionPool;  // Controller에서 Pool 주소값을 넘겨받아 생성
         
-        private void ActivateSkill()
-        {
-            float radius = Attributes[HarudesuyoAttributeType.Radius].CurrentValue;
-            Collider2D[] enemies = Physics2D.OverlapCircleAll(transform.position, radius);
+        public GameObject projectile;   // 떨어지는 미사일 이미지
 
-            if (enemies.Length == 0)
+        public void Initialize(float fallTime, float radius, float damage, Transform target, ObjectPool<Harudesuyo> pool, ObjectPool<GameObject> explosionPool)
+        {
+            _fallTime = fallTime;
+            _radius = radius;
+            _damage = damage;
+            _target = target;
+            _pool = pool;
+            _explosionPool = explosionPool;
+            
+            projectile.SetActive(true);
+            StartCoroutine(Fall());
+        }
+
+        // _fallTime에 걸쳐 폭탄이 떨어짐
+        private IEnumerator Fall()
+        {
+            Vector3 startPos = projectile.transform.position;
+            Vector3 targetPos = _target.position;
+            
+            float journeyLength = Vector3.Distance(startPos, targetPos);
+            float journeyTime = _fallTime;
+            float startTime = Time.time;
+
+            while (projectile.transform.position.y < targetPos.y)
             {
-                return;
+                float distCovered = (Time.time - startTime) * (journeyLength / journeyTime);
+                float fractionOfJourney = distCovered / journeyLength;
+                
+                projectile.transform.position = Vector3.Lerp(startPos, targetPos, fractionOfJourney);
+                yield return null;
             }
             
-            float targetCount = Attributes[HarudesuyoAttributeType.TargetCount].CurrentValue;
-            StartCoroutine(SpawnBombs(enemies.Take((int)targetCount).ToArray()));
+            projectile.transform.position = targetPos;
+            Explode();
         }
 
-        IEnumerator SpawnBombs(Collider2D[] targets)
+        private void Explode()
         {
-            return null;
+            GameObject effect = _explosionPool.Get();
+            effect.transform.position = projectile.transform.position;
+            effect.GetComponent<HarudesuyoExplosionEffect>().Play();
+            
+            Collider2D[] hitObjects = Physics2D.OverlapCircleAll(effect.transform.position, _radius, LayerMask.GetMask("Enemy"));
+            foreach (Collider2D hit in hitObjects)
+            {
+                Enemy enemy = hit.GetComponentInParent<Enemy>();
+                if (enemy)
+                {
+                    enemy.TakeDamage(_damage, enemy, effect);
+                }
+            }
+            
+            // 폭발 완료 후 오브젝트 풀에 반환
+            _pool.Release(this);
+            projectile.SetActive(false);
+        }
+
+        private void OnDisable()
+        {
+            // TODO: 폭탄 이미지 및 기타 상태 초기화
+            projectile.SetActive(false);
         }
     }
 }
