6fbda35730816acb06b9243ceea8f75020c2d911
9kyo-hwang <ryghkd507@naver.com>
2025-03-27 18:02:30 +0900

    [Feature] Projectile, FlagComponent 적용 중

--- TouhouSurvival/Assets/Scripts/Ability/Weapons/Harudesuyo/HarudesuyoController.cs
+++ TouhouSurvival/Assets/Scripts/Ability/Weapons/Harudesuyo/HarudesuyoController.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 using Unchord;
 using UnityEngine;
@@ -12,31 +13,33 @@ namespace Unchord
     public class HarudesuyoController : WeaponComponent
     {
         [Header("Prefab Settings")] 
-        public GameObject projectilePrefab; // 떨어지는 폭탄
+        public Harudesuyo harudesuyoPrefab; // 떨어지는 폭탄
         public GameObject explosionPrefab;  // 폭발 이펙트
         
         private const float SpawnDelayVariationRatio = 0.2f;
+        private const float SpawnHorizontalDirectionOffset = 96f;
+        private const float SpawnVerticalDirectionOffset = 270f;
         
-        #region Harudesuyo Projectile Pool
-        private ObjectPool<GameObject> _projectilePool;
-        private void ActionOnDestroyProjectile(GameObject obj)
+        #region Harudesuyo Bomb Pool
+        private ObjectPool<Harudesuyo> _bombPool;
+        private void ActionOnDestroyProjectile(Harudesuyo obj)
         {
-            Destroy(obj);
+            Destroy(obj.gameObject);
         }
 
-        private void ActionOnReleaseProjectile(GameObject obj)
+        private void ActionOnReleaseProjectile(Harudesuyo obj)
         {
-            obj.SetActive(false);
+            obj.gameObject.SetActive(false);
         }
 
-        private void ActionOnGetProjectile(GameObject obj)
+        private void ActionOnGetProjectile(Harudesuyo obj)
         {
-            obj.SetActive(true);
+            obj.gameObject.SetActive(true);
         }
 
-        private GameObject ProjectileCreateFunc()
+        private Harudesuyo ProjectileCreateFunc()
         {
-            return Instantiate(projectilePrefab);
+            return Instantiate(harudesuyoPrefab);
         }
         #endregion
         
@@ -44,22 +47,59 @@ namespace Unchord
         private ObjectPool<GameObject> _explosionPool;
         private void ActionOnDestroyExplosion(GameObject obj)
         {
-            Destroy(obj);
+            // Destroy(obj);    // 아무런 동작을 수행하지 않음
         }
 
         private void ActionOnReleaseExplosion(GameObject obj)
         {
-            obj.SetActive(false);
+            obj.gameObject.SetActive(false);
         }
 
         private void ActionOnGetExplosion(GameObject obj)
         {
-            obj.SetActive(true);
+            FlagComponent flagTable = obj.GetComponent<FlagComponent>();
+            flagTable.SetFlagFalseWithoutEvent(FLAG_SHOULD_DESTROY);
+            
+            obj.gameObject.SetActive(true);
+            
+            Animator animator = obj.GetComponent<Animator>();
+            animator.Play("HarudesuyoExplosion", -1, 0.0f);
         }
 
         private GameObject ExplosionCreateFunc()
         {
-            return Instantiate(explosionPrefab);
+            GameObject explosion = Instantiate(explosionPrefab.gameObject);
+            
+            CollisionEventEmitter emitter = explosion.transform.Find("Colliders/Circle Collider 2D")
+                .GetComponent<CollisionEventEmitter>();
+            emitter.onTriggerEnter2D += (sender, args) =>
+            {
+                GameObject enemyObject = args.targetObject;
+                if (!enemyObject)
+                {
+                    return;
+                }
+
+                Enemy enemy = enemyObject.GetComponentInParent<Enemy>();
+                if (!enemy)
+                {
+                    return;
+                }
+
+                enemy.TakeDamage(
+                    Attributes[HarudesuyoAttributeType.BombExplosionDamage].CurrentValue,
+                    GetComponentInParent<Player>(),
+                    explosion
+                );
+            };
+
+            FlagComponent flagTable = explosion.GetComponent<FlagComponent>();
+            flagTable.AddEventTrue(FLAG_SHOULD_DESTROY, component =>
+            {
+                _explosionPool.Release(component.gameObject);
+            });
+
+            return explosion;
         }
         #endregion
 
@@ -67,7 +107,7 @@ namespace Unchord
         {
             base.Awake();
 
-            _projectilePool = new ObjectPool<GameObject>(
+            _bombPool = new ObjectPool<Harudesuyo>(
                 createFunc: ProjectileCreateFunc,
                 actionOnGet: ActionOnGetProjectile,
                 actionOnRelease: ActionOnReleaseProjectile,
@@ -95,45 +135,63 @@ namespace Unchord
         protected override void UseWeapon()
         {
             base.UseWeapon();
-
-            Collider2D[] enemies = GetEnemiesOrNull();
-            if (enemies == null)
-            {
-                return;
-            }
             
-            StartCoroutine(Spawn(enemies));
+            foreach (Vector3 dropPoint in GetDropPoints())
+            {
+                StartCoroutine(SpawnBomb(dropPoint));
+            }
         }
 
-        private Collider2D[] GetEnemiesOrNull()
+        private Vector3[] GetDropPoints()
         {
-            float radius = Attributes[HarudesuyoAttributeType.Radius].CurrentValue;
-            Collider2D[] enemies = Physics2D.OverlapCircleAll(transform.position, radius);
+            int numDropPoint = (int)Attributes[HarudesuyoAttributeType.NumDropPoints].CurrentValue;
+            Vector3[] points = new Vector3[numDropPoint];
+            float bombingRange = Attributes[HarudesuyoAttributeType.BombingRange].CurrentValue;
 
-            if (enemies.Length == 0)
+            for (int i = 0; i < numDropPoint; ++i)
             {
-                return null;
+                points[i] = new Vector3(
+                    transform.position.x + Random.Range(-bombingRange, bombingRange),
+                    transform.position.y + Random.Range(-bombingRange, bombingRange),
+                    transform.position.z
+                );
             }
-            
-            float targetCount = Attributes[HarudesuyoAttributeType.TargetCount].CurrentValue;
-            return enemies.Take((int)targetCount).ToArray();
+
+            return points;
         }
 
-        IEnumerator Spawn(Collider2D[] targets)
+        private IEnumerator SpawnBomb(Vector3 dropPoint)
         {
-            foreach(Collider2D enemy in targets)
-            {
-                if (enemy)
-                {
-                    GameObject harudesuyo = _projectilePool.Get();
-                    harudesuyo.transform.position = enemy.transform.position;
-                    // TODO: initialize
-                }
+            Vector3 spawnPoint = GetBombSpawnPoint();
+            Quaternion rotation = GetBombRotation(spawnPoint);
+            float fallDelay = Attributes[HarudesuyoAttributeType.BombFallDelay].CurrentValue;
+            float explosionRadius = Attributes[HarudesuyoAttributeType.BombExplosionRadius].CurrentValue;
+            float explosionDamage = Attributes[HarudesuyoAttributeType.BombExplosionDamage].CurrentValue;
+            
+            Harudesuyo bomb = _bombPool.Get();
+            bomb.Initialize(fallDelay, explosionRadius, explosionDamage, rotation, spawnPoint, dropPoint, _bombPool, _explosionPool);
 
-                float spawnDelay = Attributes[HarudesuyoAttributeType.BombSpawnDelay].CurrentValue;
-                float randomFactor = Random.Range(1f - SpawnDelayVariationRatio, 1f + SpawnDelayVariationRatio);
-                yield return new WaitForSeconds(spawnDelay * randomFactor);
-            }
+            float spawnDelay = Attributes[HarudesuyoAttributeType.BombSpawnDelay].CurrentValue;
+            float randomFactor = Random.Range(1f - SpawnDelayVariationRatio, 1f + SpawnDelayVariationRatio);
+            yield return new WaitForSeconds(spawnDelay * randomFactor);
+        }
+
+        private Vector3 GetBombSpawnPoint()
+        {
+            return Random.Range(0, 3) switch
+            {
+                0 => new Vector3(transform.position.x - SpawnHorizontalDirectionOffset, transform.position.y + SpawnVerticalDirectionOffset, transform.position.z),
+                1 => new Vector3(transform.position.x, transform.position.y + SpawnVerticalDirectionOffset, transform.position.z),
+                2 => new Vector3(transform.position.x + SpawnHorizontalDirectionOffset, transform.position.y + SpawnVerticalDirectionOffset, transform.position.z),
+                _ => Vector3.zero
+            };
+        }
+
+        private Quaternion GetBombRotation(Vector3 spawnPoint)
+        {
+            if (spawnPoint.x < transform.position.x) return Quaternion.Euler(0f, 0f, 45f);  // 좌측 상공
+            if (spawnPoint.x > transform.position.x) return Quaternion.Euler(0f, 0f, -45f); // 우측 상공
+            return Quaternion.Euler(0f, 0f, 0f);
         }
     }
 }
