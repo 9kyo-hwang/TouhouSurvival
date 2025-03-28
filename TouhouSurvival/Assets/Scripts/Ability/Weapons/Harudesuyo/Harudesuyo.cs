62f09e56f6f8bfdd084730fa70256159b1e8eb75
9kyo-hwang <ryghkd507@naver.com>
2025-03-28 17:50:22 +0900

    [Feature] 봄이에요 95% 완성
    최초 발동 시 쿨타임 오적용 문제 해결 필요

--- TouhouSurvival/Assets/Scripts/Ability/Weapons/Harudesuyo/Harudesuyo.cs
+++ TouhouSurvival/Assets/Scripts/Ability/Weapons/Harudesuyo/Harudesuyo.cs
@@ -9,72 +9,57 @@ namespace Unchord
     public class Harudesuyo : MonoBehaviour
     {
         private float _delay;                           // 생성 후 떨어지기까지 걸리는 지연시간
-        private float _radius;                          // 폭발 범위
-        private float _damage;                          // 폭발 데미지
-        private Quaternion _rotation;                   // 폭탄 이미지 회전값
+        private Vector3 _rotation;                   // 폭탄 이미지 회전값
         private Vector3 _spawnPoint;                    // 폭탄 생성 지점
         private Vector3 _dropPoint;                     // 폭탄 낙하 지점
-        private ObjectPool<Harudesuyo> _pool;           // 생성 후 Pool에 반환시키기 위해 참조만 들고 있음
+        private ObjectPool<Harudesuyo> _pool;
         private ObjectPool<GameObject> _explosionPool;  // Controller에서 Pool 주소값을 넘겨받아 생성
         
-        public GameObject bombImage;                    // 떨어지는 폭탄 이미지
         public float bombFallSpeed = 5f;                // 폭탄 떨어지는 속도
         
-        public void Initialize(float fallDelay, float explosionRadius, float explosionDamage, 
-            Quaternion rotation, Vector3 spawnPoint, Vector3 dropPoint, 
-            ObjectPool<Harudesuyo> bombPool, ObjectPool<GameObject> explosionPool)
+        public void Initialize(float fallDelay, Vector3 rotation, Vector3 spawnPoint, Vector3 dropPoint, ObjectPool<Harudesuyo> pool, ObjectPool<GameObject> explosionPool)
         {
             _delay = fallDelay;
-            _radius = explosionRadius;
-            _damage = explosionDamage;
             _rotation = rotation;
             _spawnPoint = spawnPoint;
             _dropPoint = dropPoint;
-            _pool = bombPool;
+            _pool = pool;
             _explosionPool = explosionPool;
             
-            bombImage.SetActive(true);
-            StartCoroutine(Fall());
+            StartCoroutine(FallBomb());
         }
 
         // _delay 시간 후 dropPoint를 향해 폭탄 낙하
-        private IEnumerator Fall()
+        private IEnumerator FallBomb()
         {
-            yield return new WaitForSeconds(_delay);
-
             LinearProjectile projectile = GetComponent<LinearProjectile>();
-            projectile.transform.localPosition = Vector3.zero;
-            projectile.transform.rotation = _rotation;
+            projectile.transform.position = _spawnPoint;
+            projectile.transform.rotation = Quaternion.Euler(_rotation + new Vector3(0f, 0f, 90f));
             projectile.OriginPosition = _spawnPoint;
+            projectile.ProjectileSpeed = 0.0f;
+            projectile.ProjectileDirection = Vector2.zero;
+            
+            yield return new WaitForSeconds(_delay);
+            
+            projectile.ProjectileDirection = Projectile.GetTargetDirectionVector(_spawnPoint, _dropPoint, 0.0f);
             projectile.ProjectileSpeed = bombFallSpeed;
 
-            projectile.ProjectileDirection = Projectile.GetTargetDirectionVector(_spawnPoint, _dropPoint, 0.0f);
-        }
-
-        private void Explode()
-        {
-            GameObject effect = _explosionPool.Get();
-            effect.transform.position = _dropPoint;
-            effect.GetComponent<HarudesuyoExplosionEffect>().Play();
-            
-            Collider2D[] hitObjects = Physics2D.OverlapCircleAll(_dropPoint, _radius, LayerMask.GetMask("Enemy"));
-            foreach (Collider2D hit in hitObjects)
+            float journeyLength = (_spawnPoint - _dropPoint).sqrMagnitude;
+            while (journeyLength > (projectile.transform.position - _spawnPoint).sqrMagnitude)
             {
-                Enemy enemy = hit.GetComponentInParent<Enemy>();
-                if (enemy)
-                {
-                    Player player = gameObject.GetComponentInParent<Player>();
-                    enemy.TakeDamage(_damage, player, effect);
-                }
+                yield return null;
             }
-            
-            bombImage.SetActive(false);
+
+            PlayExplosionEffect();
         }
 
-        private void OnDisable()
+        private void PlayExplosionEffect()
         {
-            // TODO: 폭탄 이미지 및 기타 상태 초기화
-            bombImage.SetActive(false);
+            _pool.Release(this);
+            
+            GameObject effect = _explosionPool.Get();   // Animation Play
+            effect.transform.position = _dropPoint;
+            effect.GetComponent<DotProjectile>().OriginPosition = _dropPoint;
         }
     }
 }
