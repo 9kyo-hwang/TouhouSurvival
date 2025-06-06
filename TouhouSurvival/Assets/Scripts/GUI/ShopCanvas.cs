7417b902ae59e01cab6140d6c74217008faedbeb
9kyo-hwang <ryghkd507@naver.com>
2025-06-06 19:49:15 +0900

    [Feature] 보유한 골드를 기반으로 포인트 교환 및 사용 베이스 로직 적용

--- TouhouSurvival/Assets/Scripts/GUI/ShopCanvas.cs
+++ TouhouSurvival/Assets/Scripts/GUI/ShopCanvas.cs
@@ -1,88 +1,163 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 namespace Unchord
 {
-    internal class PointPanel
+    public class ShopData
     {
-        private Text _quantity;
-        private Button _exchange;
-        private Button _reset;
+        public int Gold {get; private set;} = 999999;   // 현재 플레이어가 보유한 골드량
+        public int InvestedPoints {get; private set;} = 0; // 플레이어가 지금까지 투자한 포인트량
+        public int ExchangedPoints {get; private set;} = 0; // 현재 플레이어가 교환한 총 포인트량
+        public int InvestablePoints => ExchangedPoints - InvestedPoints;   // 플레이어가 투자할 수 있는 남은 포인트량
 
-        public PointPanel(Transform panel)
+        public event System.Action<int> GoldChanged;
+        public event System.Action<int, int> PointsChanged;
+
+        private const int BASE_EXCHANGE_RATE = 100;
+        private const float EXCHANGE_RATE_INCREASE = 0.1f;
+        private int ExcahngeRate => Mathf.RoundToInt(BASE_EXCHANGE_RATE * (1 + (ExchangedPoints * EXCHANGE_RATE_INCREASE)));    // 지금까지 교환한 포인트량에 비례해 증가하는 골드 환전량
+
+        public bool TryExchangePoints(int points = 1)
         {
-            _quantity = panel.Find("QuantityText").GetComponent<Text>();
-            _exchange = panel.Find("ExchangeButton").GetComponent<Button>();
-            _reset = panel.Find("ResetButton").GetComponent<Button>();
-            
-            _exchange.onClick.AddListener(OnClick_Exchange);
-            _reset.onClick.AddListener(OnClick_Reset);
+            int requiredGold = points * ExcahngeRate;
+            if(Gold < requiredGold)
+            {
+                return false;
+            }
+
+            Gold -= requiredGold;
+            ExchangedPoints += points;
+
+            GoldChanged?.Invoke(Gold);
+            PointsChanged?.Invoke(InvestablePoints, ExchangedPoints);
+
+            return true;
         }
 
-        private void OnClick_Exchange()
+        public void InvestPoint()
         {
-            
+            InvestedPoints++;
+            PointsChanged?.Invoke(InvestablePoints, ExchangedPoints);
         }
 
-        private void OnClick_Reset()
+        public void RefundPoint()
         {
-            
+            InvestedPoints--;
+            PointsChanged?.Invoke(InvestablePoints, ExchangedPoints);
+        }
+
+        public void ResetInvestedPoints(int totalRefund)
+        {
+            InvestedPoints -= totalRefund;
+            if(InvestedPoints < 0) InvestedPoints = 0;
+            PointsChanged?.Invoke(InvestablePoints, ExchangedPoints);
         }
     }
 
-    internal class GoldPanel
-    {
-        private Image _image;
-        private Text _text;
-
-        public GoldPanel(Transform panel)
-        {
-            _image = panel.Find("Image").GetComponent<Image>();
-            _text = panel.Find("Text").GetComponent<Text>();
-        }
-        
-        
-    }
-    
     public class ShopCanvas : UnchordCanvas
     {
-        private Button _btnBack;
-        private PointPanel _panelPoint;
-        private GoldPanel _panelGold;
+        [SerializeField] private Text goldText;
+        [SerializeField] private Text pointsText;
+        [SerializeField] private Button backButton;
+        [SerializeField] private Button exchangeButton;
+        [SerializeField] private Button resetButton;
+        [SerializeField] private Transform itemContainer;
 
-        private UnchordCanvas _prevCanvas;
+        private UnchordCanvas _previousCanvas;
+        private ShopData _shopData;
+        private List<ShopItemView> _itemViews = new List<ShopItemView>();
 
         protected override void Awake()
         {
             base.Awake();
 
-            _btnBack = transform.Find("BackgroundPanel/ExitButton").GetComponent<Button>();
-            _panelPoint = new PointPanel(transform.Find("BackgroundPanel/PointPanel"));
-            _panelGold = new GoldPanel(transform.Find("BackgroundPanel/GoldPanel"));
-            
-            _btnBack.onClick.AddListener(OnClick_BackButton);
+            _shopData = new ShopData();
+
+            BindButtonEvents();
+            BindShopDataEvents();
+            InitializeItemViews();
+            UpdateGoldDisplay(_shopData.Gold);
+            UpdatePointsDisplay(_shopData.InvestablePoints, _shopData.ExchangedPoints);
         }
 
-        public override void Show()
+        private void BindButtonEvents()
         {
-            base.Show();
+            backButton.onClick.AddListener(OnBackClicked);
+            exchangeButton.onClick.AddListener(OnExchangeClicked);
+            resetButton.onClick.AddListener(OnResetClicked);
+        }
+
+        private void BindShopDataEvents()
+        {
+            _shopData.GoldChanged += UpdateGoldDisplay;
+            _shopData.PointsChanged += UpdatePointsDisplay;
+        }
+
+        private void InitializeItemViews()
+        {
+            foreach(Transform child in itemContainer)
+            {
+                var itemView = child.GetComponent<ShopItemView>();
+                if(itemView == null)
+                {
+                    itemView = child.gameObject.AddComponent<ShopItemView>();
+                }
+
+                var itemData = new ShopItemData("Name");
+                itemView.Initialize(itemData, _shopData);
+                _itemViews.Add(itemView);
+            }
+        }
+
+        private void UpdateGoldDisplay(int gold)
+        {
+            goldText.text = gold.ToString("D6");
+        }
+
+        private void UpdatePointsDisplay(int investable, int exchanged)
+        {
+            pointsText.text = $"{investable}/{exchanged}";
+        }
+
+        private void OnExchangeClicked()
+        {
+            if(_shopData.TryExchangePoints())
+            {
+                // UI는 이벤트로 자동 갱신됨
+            }
+        }
+
+        private void OnResetClicked()
+        {
+            int totalRefund = 0;
+            foreach(var itemView in _itemViews)
+            {
+                totalRefund += itemView.ResetLevel();
+            }
+
+            _shopData.ResetInvestedPoints(totalRefund);
+        }
+
+        private void OnBackClicked()
+        {
+            this.Hide();
+
+            Debug.Assert(_previousCanvas != null);
+
+            _previousCanvas.Show();
+        }
+
+        public void ReserveReturnCanvas(UnchordCanvas returnCanvas)
+        {
+            _previousCanvas = returnCanvas;
         }
 
         public override void Hide()
         {
             base.Hide();
-        }
-
-        public void ReserveReturnCanvas(UnchordCanvas canvas)
-        {
-            _prevCanvas = canvas;
-        }
-
-        private void OnClick_BackButton()
-        {
-            Hide();
-            Debug.Assert(_prevCanvas is not null);
-            _prevCanvas.Show();
         }
     }
 }
---
+++ TouhouSurvival/Assets/Scripts/GUI/ShopItemView.cs
@@ -0,0 +1,124 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Unchord
+{
+    public class ShopItemView : MonoBehaviour
+    {
+        [SerializeField] private Image icon;
+        [SerializeField] private Text titleText;
+        [SerializeField] private Text levelText;
+        [SerializeField] private Button upgradeButton;
+        [SerializeField] private Button downgradeButton;
+
+        private ShopItemData _itemData;
+        private ShopData _shopData;
+
+        public void Initialize(ShopItemData itemData, ShopData shopData)
+        {
+            _itemData = itemData;
+            _shopData = shopData;
+
+            _itemData.LevelChanged += UpdateDisplay;
+            _shopData.PointsChanged += OnPointsChanged;
+
+            UpdateDisplay();
+            BindEvents();
+        }
+        
+        private void BindEvents()
+        {
+            upgradeButton.onClick.AddListener(OnUpgradeClicked);
+            downgradeButton.onClick.AddListener(OnDowngradeClicked);
+        }
+        
+        private void UpdateDisplay()
+        {
+            titleText.text = _itemData.Title;
+            levelText.text = $"{_itemData.CurrentLevel}/{_itemData.MaxLevel}";
+
+            upgradeButton.interactable = _shopData.InvestablePoints > 0 && _itemData.CurrentLevel < _itemData.MaxLevel;
+            downgradeButton.interactable = _itemData.CurrentLevel > 0;
+        }
+
+        private void OnUpgradeClicked()
+        {
+            if(_shopData.InvestablePoints > 0 && _itemData.TryUpgrade())
+            {
+                _shopData.InvestPoint();
+            }
+        }
+
+        private void OnDowngradeClicked()
+        {
+            if(_itemData.TryDowngrade())
+            {
+                _shopData.RefundPoint();
+            }
+        }
+
+        public int ResetLevel()
+        {
+            int refund = _itemData.CurrentLevel;
+            if(refund > 0)
+            {
+                for(int i=0; i<refund; ++i)
+                {
+                    _shopData.RefundPoint();
+                }
+
+                _itemData.ForceSetLevel(0);
+                UpdateDisplay();
+            }
+            return refund;
+        }
+
+        private void OnPointsChanged(int investable, int exchanged)
+        {
+            UpdateDisplay();
+        }
+
+        private void OnDestroy()
+        {
+            if(_itemData != null) _itemData.LevelChanged -= UpdateDisplay;
+            if(_shopData != null) _shopData.PointsChanged -= OnPointsChanged;
+        }
+    }
+
+    public class ShopItemData
+    {
+        public string Title { get; }
+        public int CurrentLevel { get; private set; } = 0;
+        public int MaxLevel { get; } = 5;        
+        public event System.Action LevelChanged;
+
+        public ShopItemData(string title)
+        {
+            Title = title;
+        }
+        
+        public bool TryUpgrade()
+        {
+            if (CurrentLevel >= MaxLevel) return false;
+            
+            CurrentLevel++;
+            LevelChanged?.Invoke();
+            return true;
+        }
+        
+        public bool TryDowngrade()
+        {
+            if (CurrentLevel <= 0) return false;
+            
+            CurrentLevel--;
+            LevelChanged?.Invoke();
+            return true;
+        }
+
+        public void ForceSetLevel(int level)
+        {
+            CurrentLevel = level;
+            LevelChanged?.Invoke();
+        }
+    }
+}
