7c8e52de396ed895933aefad083481809901f266
YellowGreenLime <nlime3141592@naver.com>
2025-07-06 02:03:18 +0900

    [Refactor] 레벨업 화면 구성 변경

--- TouhouSurvival/Assets/Scripts/GUI/LevelUpCanvas.cs
+++ TouhouSurvival/Assets/Scripts/GUI/LevelUpCanvas.cs
@@ -1,124 +1,106 @@
-using System.Text.RegularExpressions;
-using TMPro;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace Unchord
 {
     public class LevelUpCanvas : UnchordCanvas
     {
-        public int SelectionCount => _shownAbilityCount;
         public int SelectedIndex { get; private set; } = -1;
 
-        private Transform _selectionButtonContainer;
-        private Transform _noEntryText;
-        private int _shownAbilityCount;
+        #region Inspector Properties
+        public Sprite imgWeaponPanel;
+        public Sprite imgPassivePanel;
+        public IntegerCounterFontSO levelFont;
+        #endregion
+
+        private List<SelectionButton> _btnSelections;
+        private int _btnEnabledCount;
+        private Transform _selectionPrefab;
 
         protected override void Awake()
         {
             base.Awake();
 
-            _selectionButtonContainer = transform.Find("Selection Buttons");
-            _noEntryText = _selectionButtonContainer.Find("NoEntry");
-            _noEntryText.GetComponent<Button>().onClick.AddListener(OnNoEntryButtonClick);
+            _btnSelections = new List<SelectionButton>(4);
+            _selectionPrefab = transform.Find("Selection Buttons/__SelectionButtonPrefab");
+
+            for (int i = 0; i < _btnSelections.Capacity; ++i)
+            {
+                _btnSelections.Add(new SelectionButton(_selectionPrefab, i, OnSelectionButtonClick));
+                _btnSelections[i].Disable();
+            }
+
+            _btnEnabledCount = 0;
         }
 
-        protected override void OnEnable()
+        public override void Show()
         {
-            base.OnEnable();
+            base.Show();
 
             SelectedIndex = -1;
 
-            UnityEngine.Debug.Assert(_shownAbilityCount >= 0);
-            _noEntryText.gameObject.SetActive(_shownAbilityCount == 0);
+            s_uiManager.SingleColorCanvas0.LayerBackOf(this);
+            s_uiManager.SingleColorCanvas0.Show();
+        }
+
+        public override void Hide()
+        {
+            base.Hide();
+
+            s_uiManager.SingleColorCanvas0.Hide();
         }
 
         public void Clear()
         {
-            for (int i = 1; i <= _shownAbilityCount; ++i)
+            while (_btnEnabledCount > 0)
             {
-                string buttonName = GetButtonName(i);
-                GameObject button = _selectionButtonContainer.Find(buttonName).gameObject;
-                button.SetActive(false);
+                --_btnEnabledCount;
+
+                _btnSelections[_btnEnabledCount].Disable();
             }
-
-            _shownAbilityCount = 0;
         }
 
-        public void Add(AbilityComponent ability)
+        public void AddAbility(AbilityComponent ability)
         {
-            int index = ++_shownAbilityCount;
-            string buttonName = GetButtonName(index);
-            Transform buttonTransform = _selectionButtonContainer.Find(buttonName);
+            int i = _btnEnabledCount;
+            SelectionButton button = _btnSelections[i];
 
-            if (buttonTransform == null)
-                buttonTransform = CreateNewButton(index);
+            if (ability is WeaponComponent)
+                button.SetPanel(imgWeaponPanel);
+            else if (ability is PassiveComponent)
+                button.SetPanel(imgPassivePanel);
 
-            buttonTransform.gameObject.SetActive(true);
+            button.SetIcon(ability.DisplayIcon);
+            button.SetLevel(ability.CurrentLevel + 1, levelFont);
+            button.SetSelectionName(ability.DisplayName);
+            button.SetDescription("description here.");
+            button.Enable();
 
-            this.GetIcon(buttonTransform).sprite = ability.DisplayIcon;
-            this.GetName(buttonTransform).text = ability.DisplayName;
-            this.GetDescription(buttonTransform).text = "level up description here.";
+            ++_btnEnabledCount;
+
+            UnityEngine.Debug.Assert(_btnEnabledCount <= _btnSelections.Capacity);
         }
 
-        private Transform CreateNewButton(int index)
+        public void AddNoEntry()
         {
-            Transform prefab = _selectionButtonContainer.Find(GetButtonName(0));
-            Transform newButton = GameObject.Instantiate(prefab);
+            int i = _btnEnabledCount;
+            SelectionButton button = _btnSelections[i];
 
-            newButton.SetParent(_selectionButtonContainer, false);
-            newButton.name = GetButtonName(index);
+            button.SetPanel(imgPassivePanel);
+            button.SetIcon(null);
+            button.SetLevel(0, levelFont);
+            button.SetSelectionName("강화 완료");
+            button.SetDescription("모든 능력을\n강화했습니다.");
+            button.Enable();
 
-            Button button = newButton.GetComponent<Button>();
-            button.onClick.AddListener(() => OnSelectionButtonClick(newButton));
+            ++_btnEnabledCount;
 
-            return newButton;
+            UnityEngine.Debug.Assert(_btnEnabledCount <= _btnSelections.Capacity);
         }
 
-        private Image GetIcon(Transform selection)
+        private void OnSelectionButtonClick(int index)
         {
-            return selection.Find("Icon").GetComponent<Image>();
-        }
-
-        private TextMeshProUGUI GetName(Transform selection)
-        {
-            return selection.Find("Name").GetComponent<TextMeshProUGUI>();
-        }
-
-        private TextMeshProUGUI GetDescription(Transform selection)
-        {
-            return selection.Find("Description").GetComponent<TextMeshProUGUI>();
-        }
-
-        private void OnNoEntryButtonClick()
-        {
-            SelectedIndex = 0;
-        }
-
-        private void OnSelectionButtonClick(Transform buttonTransform)
-        {
-            SelectedIndex = GetButtonIndex(buttonTransform);
-        }
-
-        private string GetButtonName(int index)
-        {
-            return $"Selection Button ({index})";
-        }
-
-        private int GetButtonIndex(Transform buttonTransform)
-        {
-            string buttonName = buttonTransform.name;
-            string pattern = @"^Selection Button \((\d+)\)$";
-            int index;
-
-            Match match = Regex.Match(buttonName, pattern);
-
-            if (!match.Success || !int.TryParse(match.Groups[1].Value, out index))
-                return -1;
-
-            UnityEngine.Debug.Assert(index > 0);
-
-            return index - 1;
+            SelectedIndex = index;
         }
     }
 }
