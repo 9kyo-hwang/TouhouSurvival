b3cbe5bda98a8cf0506da3c864c625f5fb8317b7
YellowGreenLime <nlime3141592@naver.com>
2025-07-19 01:45:58 +0900

    [Fix] Smooth Follow 기능의 증가/감소 계수 분리

--- TouhouSurvival/Assets/Scripts/GameManager/PlayerCamera.cs
+++ TouhouSurvival/Assets/Scripts/GameManager/PlayerCamera.cs
@@ -4,10 +4,19 @@ namespace Unchord
 {
     public class PlayerCamera : MonoBehaviour
     {
-        public float rMax = 3.0f;
-        public float sMax = 5.0f;
+        public float maxRadius = 1.5f; // 타겟이 카메라 중심에서 이 반지름 내에 반드시 존재함.
 
-        public float w = 0.2f;
+        // TODO: 코드 분석 필요.
+        // 타겟의 현재 이동 속도가 clampingTargetSpeedThreshold에 도달할 때 타겟이 카메라 중심에서 maxRadius 만큼 떨어지도록 코드를 설계하는 것이 목표였다.
+        // 현재 계산컨대, 타겟의 이동 속도가 radiusMax * wIncrease / (Time.fixedDeltaTime * (1 - wIncrease))에 도달했을 때 maxRadius에 도달하는 것으로 계산됨.
+        // 계산을 면밀히 검토해 볼 필요 있음.
+        public float clampingTargetSpeedThreshold = 5.0f;
+
+        [Range(0.01f, 1.0f)]
+        public float wIncrease = 0.065f;
+
+        [Range(0.01f, 1.0f)]
+        public float wDecrease = 0.055f;
 
         private GameManager _gameManager;
 
@@ -61,23 +70,25 @@ namespace Unchord
 
             if (currentSpeed > 0.0f)
             {
-                _moveSum += deltaPosition;
-                _moveSum *= (1.0f - w);
+                UnityEngine.Debug.Assert(wIncrease > 0.0f && wIncrease <= 1.0f);
 
-                float r = currentSpeed * (rMax / sMax);
+                _moveSum += deltaPosition;
+                _moveSum *= (1.0f - wIncrease);
+
+                float r = currentSpeed * (maxRadius / clampingTargetSpeedThreshold);
                 r = Mathf.Round(r * 10.0f) / 10.0f;
 
-                if (r > rMax)
-                    r = rMax;
+                if (r > maxRadius)
+                    r = maxRadius;
 
                 if (_moveSum.magnitude > r)
                     _moveSum = _moveSum.normalized * r;
             }
             else
             {
-                UnityEngine.Debug.Assert(w > 0.0f && w <= 1.0f);
+                UnityEngine.Debug.Assert(wDecrease > 0.0f && wDecrease <= 1.0f);
 
-                _moveSum *= (1.0f - w);
+                _moveSum *= (1.0f - wDecrease);
             }
         }
 
