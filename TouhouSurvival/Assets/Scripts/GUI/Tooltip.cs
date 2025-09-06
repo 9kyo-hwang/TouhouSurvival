87a5a5695af6824c1e6f7225894bac933659b6ba
YellowGreenLime <nlime3141592@naver.com>
2025-09-06 14:17:18 +0900

    [Feature] 툴팁 박스 Pivot/Offset 조절 기능 추가

--- TouhouSurvival/Assets/Scripts/GUI/Tooltip.cs
+++ TouhouSurvival/Assets/Scripts/GUI/Tooltip.cs
@@ -6,20 +6,35 @@ namespace Unchord
     public class Tooltip : MonoBehaviour
     {
         private RectTransform _background;
+        private Vector2 _pivot;
+        private Vector2 _offset;
         private Text _description;
 
         private void Start()
         {
             _background = transform.GetChild(0).GetComponent<RectTransform>();
+            _pivot = _background.pivot;
+            _offset = Vector2.zero;
             _description = _background.GetChild(0).GetComponent<Text>();
 
             gameObject.SetActive(false);
         }
 
+        public void SetPivot(Vector2 pivot)
+        {
+            _pivot = pivot;
+        }
+
+        public void SetOffset(Vector2 offset)
+        {
+            _offset = offset;
+        }
+
         public void Show(string description, Vector2 position)
         {
             _description.text = description;
-            _background.position = position;
+            _background.position = _offset + position;
+            _background.pivot = _pivot;
             
             gameObject.SetActive(true);
         }
